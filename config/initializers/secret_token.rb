92b8c942892952fec680a7b9db0d34fbbbdcb8c0
Michael Ries <michael@riesd.com>
2011-02-20 17:47:40 -0800

    adding a frehs rails generation with rails 3.0.1

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ladder::Application.config.secret_token = '921f7c2669457355ec8dca8d50ee912e249a5aeb96be4b2fe1b3435697572684b41b24de6a6f8975fcb5e6daadb0813997983e291487781675855be78cdbbc2c'
