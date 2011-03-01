e198789286397f1312f8805bac7aa18c7296e304
Michael Ries <michael@riesd.com>
2011-02-28 23:46:44 -0800

    adding a basic layout and simplifying the layout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -11,12 +11,23 @@ class SessionsController < ApplicationController
   # POST /sessions #login
   def create
     @session = Session.new(params[:session])
-
-    
+    res = Player.by_name :key => @session.username
+    if res.nil? or res.first.password != @session.password then
+      @session.errors.add :username, "the username/password supplied does not match any known user"
+      respond_to do |format|
+        format.html { render :action => "new" }
+      end
+    else
+      session[:login] = res.first["_id"]
+      redirect_to :controller => "players", :action => "index"
+    end
   end
 
   # DELETE /sessions/1 - #logout
   def destroy
-    
+    unless session[:login].nil? then
+      session[:login] = nil
+    end
+    redirect_to :controller => "players", :action => "index"
   end
 end
