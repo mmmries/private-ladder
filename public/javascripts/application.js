da1fd7ff0d45568cd1ed8e6b1d4a628844696dd8
Michael Ries <michael@riesd.com>
2011-03-24 01:19:13 -0700

    getting some basic backbone up and running to make the UI more interactive

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,11 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+
+var Player = Backbone.Model.extend({
+   promptName: function(){
+        var new_name = prompt("Please enter the new name: ");
+        this.set({name: new_name});
+   }
+});
+
